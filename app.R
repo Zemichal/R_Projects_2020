6d143932451f929151c1e543616f7c641fb1caf4
Zemichal <62442999+Zemichal@users.noreply.github.com>
2020-04-23 13:51:00 -0400

    app.R

--- app.R
+++ app.R
@@ -14,8 +14,10 @@ ui <- fluidPage(
   titlePanel("Above + Beyond Charting Tool"),
   mainPanel(tabsetPanel(
     tabPanel("Vertical Chart", plotOutput("chartv", width = "100%", height="700px")),
-    tabPanel("Horizontial Chart", plotOutput("charth", width = "100%", height="700px")))),
+    tabPanel("Horizontial Chart", plotOutput("charth", width = "100%", height="700px")),
+    tabPanel("Nine Box Chart", plotOutput("nineB", width = "100%", height = "700px")))),
   fluidRow(column(2,wellPanel(
+    h5("Set these Parameters First then Update Chart"),
     selectInput("xl", "Please Select X Axis Label",choices = c(" ","Enterprsing Potential","Achievement Potential",
                                                                "Independence Potential","People Orientation","Analytical Orientation", 
                                                                "Comfort With Conflict", "Emotional Quotient"), selected = " "),
@@ -27,8 +29,8 @@ ui <- fluidPage(
     selectInput("y", "Please Select Y Axis", choices =c("EP","AP","IP","PO",
                                                         "AO", "CWC", "EQ","NULL")),
     
-    selectInput("fv", "Please Select Vertical Facet", choices =c("Level", "Performance","NULL"), selected = "NULL"),
-    selectInput("fh", "Please Select Horizontal Facet", choices =c("Level", "Performance","NULL"), selected = "NULL")),
+    selectInput("fv", "Please Select Vertical Facet", choices =c("Level", "Performance","Benchmark Potential","NULL"), selected = "NULL"),
+    selectInput("fh", "Please Select Horizontal Facet", choices =c("Level", "Performance","Benchmark Potential","NULL"), selected = "NULL")),
     actionButton("button","Update Chart"))),
   
   column(3,wellPanel(
@@ -39,24 +41,29 @@ ui <- fluidPage(
     textInput("h", "Set the High Color", "Black"),
     textInput("m", "Set the Mid Color", "Black"),
     textInput("l", "Set the Low Color", "Black"),
+    h5("When setting the Midpoint for the H.Chart use the inverse of the number"),
     numericInput("mp", "Set the Midpoint Value", 15))),
   
   fluidRow(column(4,wellPanel(
+    h5("Use this to set the Range for the H. Chart"),
     numericRangeInput("scalexr", "Please Select Range For X Axis",
-                      value = c(-100,115)),
+                      value = c(-115,115)),
+    h5("Use this to remove ticks on X Axis"),
     sliderInput("scalex1", "Please Select Start For X Axis", 
                 min = -115, max = 115, value = -100, width = 1000),
     sliderInput("scalex2", "Please Select End For X Axis", 
                 min = -115, max = 115, value = 115, width = 1000),
-    h5("Sample Text May Use to Detail Inputs"),
+    h5("Use this to set the Range for the V. Chart"),
     numericRangeInput("scaleyr", "Please Select Range For Y Axis",
-                      value = c(-100,115)),
+                      value = c(-115,115)),
+    h5("Use this to remove ticks on Y Axis"),
     sliderInput("scaley1", "Please Select Start For Y Axis", 
                 min = -115, max = 115, value = -100, width = 1000),
     sliderInput("scaley2", "Please Select End For Y Axis", 
                 min = -115, max = 115, value = 115, width = 1000),
     downloadButton('downloadPlot1', 'Download Vertical Plot'),
     downloadButton('downloadPlot2', 'Download Horizontial Plot'),
+    downloadButton('downloadPlot3', 'Download Nine Box')
   )))
   
   
@@ -116,12 +123,12 @@ server <- function(input, output, session) {
   
   facet_v_input <- eventReactive(input$button,{
     switch(input$fv,
-           "Performance" = as.character(eb4[,"perf"]),"Level" = as.character(eb4[,"lvls"]), "NULL" = " ")
+           "Performance" = as.character(eb4[,"perf"]),"Level" = as.character(eb4[,"lvls"]),"Benchmark Potential" = as.character(eb4[,"...46"]),"NULL" = " ")
   })
   
   facet_h_input <- eventReactive(input$button,{
     switch(input$fh,
-           "Performance" = as.character(eb4[,"perf"]),"Level" = as.character(eb4[,"lvls"]), "NULL" = " ")
+           "Performance" = as.character(eb4[,"perf"]),"Level" = as.character(eb4[,"lvls"]),"Benchmark Potential" = as.character(eb4[,"...46"]), "NULL" = " ")
   })
   
   plotOutput1 <- function(){
@@ -132,7 +139,7 @@ server <- function(input, output, session) {
       
       scale_color_gradient2(low = input$l, mid = input$m, 
                             high = input$h, midpoint = input$mp) +
-      facet_grid(.~facet_v_input()) +
+      facet_grid(facet_h_input()~facet_v_input()) +
       labs(
         x= input$xl,                       
         y= input$yl,
@@ -162,7 +169,7 @@ server <- function(input, output, session) {
       
       scale_color_gradient2(low = input$h, mid = input$m, 
                             high = input$l, midpoint = input$mp) +
-      facet_grid(facet_h_input()~.) +
+      facet_grid(facet_h_input()~facet_v_input()) +
       labs(
         x= input$xl,                       
         y= input$yl,
@@ -183,6 +190,30 @@ server <- function(input, output, session) {
              legend.text = element_blank())
     
   }
+  plotOutput3 <- function(){
+    ggplot(data = eb4) +
+      
+      facet_grid(facet_h_input()~facet_v_input()) +
+      labs(
+        x= input$xl,                       
+        y= input$yl,
+        color = " ") + 
+      geom_text_repel(mapping = aes(x=axis_x_input(), y=axis_y_input(),label=as.character(...3)))+
+      guides(size = FALSE)+
+      scale_x_continuous(limits = input$scalexr,breaks = seq(from = input$scalex1, to = input$scalex2, by = input$byx)) +
+      scale_y_continuous(limits = input$scaleyr,breaks = seq(from = input$scaley1, to = input$scaley2, by = input$by)) +
+      theme( panel.background = element_rect(fill = "transparent"), # bg of the panel
+             plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+             panel.grid.major = element_blank(), # get rid of major grid
+             panel.grid.minor = element_blank(), # get rid of minor grid
+             legend.background = element_rect(fill = "transparent", color = NA), # get rid of legend bg
+             legend.box.background = element_rect(fill = "transparent", color = NA), # get rid of legend panel bg
+             legend.position = "bottom",
+             legend.key.width = unit(6.5,"cm"),
+             legend.text = element_blank())
+      
+
+  }
   
   output$chartv <- renderPlot({
     print(plotOutput1())
@@ -191,6 +222,10 @@ server <- function(input, output, session) {
   output$charth <- renderPlot({
     print(plotOutput2())
   }) 
+  
+  output$nineB <- renderPlot({
+    print(plotOutput3())
+  })
   
   output$downloadPlot1 <- downloadHandler(
     filename = "FileName.png",
@@ -207,7 +242,16 @@ server <- function(input, output, session) {
       print(plotOutput2())
       dev.off()
       
-    }) 
+    })
+  
+  output$downloadPlot3 <- downloadHandler(
+    filename = "FileName.png",
+    content = function(file) {
+      png(file, width = 1000 , height = 700, bg = "transparent" )
+      print(plotOutput3())
+      dev.off()
+      
+    })
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
